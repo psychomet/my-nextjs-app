dc00fcb0eec7c9837775d285a21606fc53c00171
Mahdi Sanatkar <sanatkar.dev@gmail.com>
2022-04-09 14:58:11 +0430

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+//
+// module.exports = nextConfig
 
-module.exports = nextConfig
+// const withAntdLess = require("next-plugin-antd-less");
+//
+// module.exports = withAntdLess({
+//   lessVarsFilePath: "./styles/antd.less",
+//   webpack(config) {
+//     return config;
+//   },
+// });
+
+const path = require("path");
+
+const pathToLessFileWithVariables = path.resolve(
+    "/styles/antd.less"
+);
+
+const withLess = require("next-with-less");
+withLess.patchNext(require("next/dist/build/webpack/config/blocks/css"));
+
+module.exports = withLess({
+  reactStrictMode: true,
+  lessLoaderOptions: {
+    // it's possible to use additionalData or modifyVars for antd theming
+    // read more @ https://ant.design/docs/react/customize-theme
+    additionalData: (content) =>
+        `${content}\n\n@import '${pathToLessFileWithVariables}';`,
+
+    // lessOptions: {
+    //   modifyVars: {
+    //     "primary-color": "#9900FF"
+    //   },
+    // },
+  },
+});
